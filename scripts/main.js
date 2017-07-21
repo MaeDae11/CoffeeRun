215b77651b9f937b269c5354d7907b1b0cebbfb2
Katie <katherinelane11@gmail.com>
2017-07-21 14:25:30 -0400

    initial uplaod

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+// local storage helps with username, timestamped tokens, etc instead of constantly asking the server over and over
+
+
+
+
+// submit event
+// var $theForm = $('[data-form="form"]');
+// $theForm.on('submit', function (event) {
+
+// });
+
+// change event
+// var $ageInput = $('[data-input="age"]'); 
+// $ageInput.on('input', function(event){
+
+// });
+
+// value
+// Get the current value of the first element in the set of matched elements or set the value of every matched element.
+// $ageInput.on('input', function (event){
+//     console.log($ageInput.val());
+// });
+
+// change value
+// $ageInput.val('');
+
+
+// set to local storage
+//The setItem() method of the Storage interface, when passed a key name and value, will add that key to the storage, or update that key's value if it already exists.
+// storage.setItem(keyName, keyValue);
+// localStorage.setItem('age', '23');
+
+
+// get local storage
+//The getItem() method of the Storage interface, when passed a key name, will return that key's value.
+// var age = localStorage.getItem('age');
+
+//The .serialize() method creates a text string in standard URL-encoded notation. It can act on a jQuery object that has selected individual form controls, such as <input>, <textarea>, and <select>: $( "input, textarea, select" ).serialize();
+
+
+// Person writes in all their stuff on form and hits submit.
+// submit will be the trigger function.
+// then loop through all the data and push it storage
