1e2e2e66eeb8323aaa67a57fe4060878853028f8
Katie <katherinelane11@gmail.com>
2017-07-21 15:26:03 -0400

    submits data to localStorage

--- scripts/main.js
+++ scripts/main.js
@@ -26,18 +26,46 @@
 
 
 // set to local storage
-//The setItem() method of the Storage interface, when passed a key name and value, will add that key to the storage, or update that key's value if it already exists.
+// The setItem() method of the Storage interface, when passed a key name and value, will add that key to the storage, or update that key's value if it already exists.
 // storage.setItem(keyName, keyValue);
 // localStorage.setItem('age', '23');
 
 
 // get local storage
-//The getItem() method of the Storage interface, when passed a key name, will return that key's value.
+// The getItem() method of the Storage interface, when passed a key name, will return that key's value.
 // var age = localStorage.getItem('age');
 
-//The .serialize() method creates a text string in standard URL-encoded notation. It can act on a jQuery object that has selected individual form controls, such as <input>, <textarea>, and <select>: $( "input, textarea, select" ).serialize();
+// The .serialize() method creates a text string in standard URL-encoded notation. It can act on a jQuery object that has selected individual form controls, such as <input>, <textarea>, and <select>: $( "input, textarea, select" ).serialize();
 
 
 // Person writes in all their stuff on form and hits submit.
 // submit will be the trigger function.
 // then loop through all the data and push it storage
+
+var $SUBMIT = $('[type="submit"]');
+var $EMAIL = $('[name="emailAddress"]');
+var $COFFEE = $('[name="coffee"]');
+var $SIZE = $('[name="size"]');
+var $FLAVOR = $('[name="flavor"]');
+var $STRENGTH = $('[name="strength"]')
+
+
+
+function submitClick(){
+    $('[data-coffee-order="form"]').on('submit', function (){
+        localStorage.setItem('coffee', $COFFEE.val());
+        localStorage.setItem('email', $EMAIL.val());
+        localStorage.setItem('size', $SIZE.val());
+        localStorage.setItem('flavor', $FLAVOR.val());
+        localStorage.setItem('strength', $STRENGTH.val());
+    }); 
+};
+
+submitClick();
+
+
+// function coffeeInfo() {
+//     var $COFFEE = $('[name="coffee"]');
+//     var name = 'coffee'
+//     localStorage.setItem('coffee', $COFFEE.val());
+// }
