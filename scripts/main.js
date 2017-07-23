c7cce7f379edc0b0037afb9f9b56ae9f413fb4a4
Katie <katherinelane11@gmail.com>
2017-07-23 17:51:11 -0400

    implements saving to API again as was commented out

--- scripts/main.js
+++ scripts/main.js
@@ -1,42 +1,5 @@
-// local storage helps with username, timestamped tokens, etc instead of constantly asking the server over and over
-
-
-
-
-// submit event
-// var $theForm = $('[data-form="form"]');
-// $theForm.on('submit', function (event) {
-
-// });
-
-// change event
-// var $ageInput = $('[data-input="age"]'); 
-// $ageInput.on('input', function(event){
-
-// });
-
-// value
-// Get the current value of the first element in the set of matched elements or set the value of every matched element.
-
-// change value
-// $ageInput.val('');
-
-
-// set to local storage
-// The setItem() method of the Storage interface, when passed a key name and value, will add that key to the storage, or update that key's value if it already exists.
-// storage.setItem(keyName, keyValue);
-// localStorage.setItem('age', '23');
-
-
-// get local storage
-// The getItem() method of the Storage interface, when passed a key name, will return that key's value.
-// var age = localStorage.getItem('age');
-
-// The .serialize() method creates a text string in standard URL-encoded notation. It can act on a jQuery object that has selected individual form controls, such as <input>, <textarea>, and <select>: $( "input, textarea, select" ).serialize();
-
-// $(document.createElement('div'));
-
-// Person writes in all their stuff on form and hits submit.
+// Overall rundown: 
+// Person types in all their answers on form and hits submit.
 // submit will be the trigger function to store in local storage AND API.
 // Get My Order button triggers function to get most recent order from Local Storage
 //  Get All Past Orders button triggers function to get all orders in API
@@ -103,10 +66,10 @@ function submitClick(){
         getSize();
         getFlavor();
         getStrength();
-        // $.post(URL, theDataz, function (resp){
-        //     console.log(resp);
+        $.post(URL, theDataz, function (resp){
+            console.log(resp);
         });
-    // }); 
+    }); 
 };
 
 
